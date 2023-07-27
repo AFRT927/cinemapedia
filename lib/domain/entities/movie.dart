e5dbc5d1f62cfd6a485c80291f60bd0f06fd1eb1
Andres <andrese-rios@utp.edu.co>
2023-07-27 17:06:34 -0500

    creacion de la entidad Movie

---
+++ lib/domain/entities/movie.dart
@@ -0,0 +1,36 @@
+class Movie {
+
+  final bool adult;
+  final String backdropPath;
+  final List<String> genreId;
+  final int id;
+  final String originalLenguage;
+  final String originalTitle;
+  final String overview;
+  final double popularity;
+  final String posterPath;
+  final DateTime releseDate;
+  final String title;
+  final bool video;
+  final double voteAverage;
+  final int voteCount;
+
+  Movie({
+    required this.adult,
+    required this.backdropPath,
+    required this.genreId,
+    required this.id,
+    required this.originalLenguage,
+    required this.originalTitle,
+    required this.overview,
+    required this.popularity,
+    required this.posterPath,
+    required this.releseDate,
+    required this.title,
+    required this.video,
+    required this.voteAverage,
+    required this.voteCount
+  });
+
+
+}
