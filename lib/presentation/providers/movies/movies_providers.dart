c8de260face10ceaa44d5696fce3c84c6b26c3b7
Andres <andrese-rios@utp.edu.co>
2023-07-28 10:37:58 -0500

    se creo el script movies_repository_provider.dart en la direccion presentation/providers/movies. En este script se programo un StateNotifierProvider que se encargara de ejeturar la funcion prooveida por el provider movieRespositoryProvider

---
+++ lib/presentation/providers/movies/movies_providers.dart
@@ -0,0 +1,32 @@
+import 'package:cinemapedia/domain/entities/movie.dart';
+import 'package:cinemapedia/presentation/providers/movies/movies_repository_provider.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+final nowPlayingMoviesProvider = StateNotifierProvider<MoviesNotifier, List<Movie>>((ref){
+final fetchMoreMovies = ref.watch(movieRespositoryProvider).getNowPlaying;
+return MoviesNotifier(fetchMoreMovies: fetchMoreMovies);
+});
+
+
+// el objetivo de definir este callback es: definir el caso de uso
+typedef MovieCallback = Future<List<Movie>> Function({int page});
+
+class MoviesNotifier extends StateNotifier <List<Movie>>{
+  
+  int currentPage = 0;
+  MovieCallback fetchMoreMovies;
+
+
+  MoviesNotifier({
+    required this.fetchMoreMovies
+    }):super([]); // inicializacion
+
+  Future<void> loadNextPage() async {
+    currentPage++;
+    //state <------modify
+    final List<Movie> movies = await fetchMoreMovies(page: currentPage);// todo get now playing
+    state = [...state, ...movies];
+  }
+
+
+}
