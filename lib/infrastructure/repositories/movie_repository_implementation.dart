e62986fe0ebcc8f2f2adad7f3710f9fb79f1c92d
Andres <andrese-rios@utp.edu.co>
2023-07-28 09:57:43 -0500

    se instalo riverpod y se creo el script movies_repository_provider.dart en la direccion presentation/providers/movies. En este script se creo un provider de solo lectura encargado de llamar la respuesta del MovieDataSource por medio del MovieRepositoryImpl

---
+++ lib/infrastructure/repositories/movie_repository_implementation.dart
@@ -0,0 +1,22 @@
+import 'package:cinemapedia/domain/datasources/movies_datasource.dart';
+import 'package:cinemapedia/domain/entities/movie.dart';
+import 'package:cinemapedia/domain/repositories/movies_repository.dart';
+
+
+/**
+ * la implementacion del repository lo que hace es
+ * llamar al datasource y del data source, llama sus
+ * metodos
+ */
+class MovieReposotoryImpl extends MoviesRespository {
+    
+    // ojo, MoviesDataSource es la clase abstracta (interface)
+    final MoviesDataSource moviesDataSource;
+
+    MovieReposotoryImpl({required this.moviesDataSource});
+
+    Future<List<Movie>> getNowPlaying({int page = 1}){
+      return moviesDataSource.getNowPlaying(page: page);
+    }
+
+}
---
+++ lib/presentation/providers/movies/movies_repository_provider.dart
@@ -0,0 +1,16 @@
+import 'package:cinemapedia/domain/entities/movie.dart';
+import 'package:cinemapedia/infrastructure/datasources/moviedb_datasource.dart';
+import 'package:cinemapedia/infrastructure/repositories/movie_repository_implementation.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+/**
+ * Ojo, este provider nunca puede ser modificado(es inmutable)
+ * por lo tanto debe ser un provider de solo lectura.
+ * Su objetivo es proporcionar la informacion neceraria a todos
+ * los demas providers para que estos puedan consultarla
+ */
+
+final movieRespositoryProvider = Provider((ref) { 
+return MovieReposotoryImpl(moviesDataSource: MovieDbDataSource());
+}
+);
